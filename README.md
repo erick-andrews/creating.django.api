70f3b906c6ea4f10c6a992376cc2f0d81489e333
Erick Andrews <andrews_erick@yahoo.com>
2022-12-17 14:34:58 -0800

    added final methods to views, updated README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # creating.django.api
-First Django API experimentation
+### First Django API experimentation:
+I created this project to experiment with CRUD operations in Django web framework. This tutorial was completed with class-based views. Function-based views will be covered in a separate repo. The steps included in this git repo were:
+
+    1. Post method
+    2. Get method
+    3. Patch method
+    4. Delete method
+
+I then tested the functionality of the methods implemented above. As part of the method implementations, we also created a `CartItemSerializer` to handle querysets and model instances by turning them into native Python datatypes and then into easily readable JSON or XML (in our case JSON). `CartItemSerializer` also provides deserialization, converting parsed data back into complex types.
--- shoppingcart/api_app/views.py
+++ shoppingcart/api_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -28,6 +28,23 @@ class CartItemViews(APIView):
             items = CartItem.objects.all()
             serializer = CartItemSerializer(items, many=True)
             return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
-    
+    # Defining patch allowing us to update records, similar to post but for class-based view
+    def patch(self, request, id=None):
+        item = CartItem.objects.get(id=id)
+        serializer = CartItemSerializer(item, # Instance of cart item
+                                        data=request.data, # data received from request
+                                        partial=True) # 
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success",
+                             "data": serializer.data})
+        else:
+            return Response({"status": "error", 
+                             "data": serializer.data})
+    # Defining a Delete method to remove records
+    def delete(self, request, id=None):
+        item = get_object_or_404(CartItem, id=id)
+        item.delete()
+        return Response({"status": "success", "data": "Item Deleted"})
 
 # Create your views here.
